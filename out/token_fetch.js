02b31c7a727767925fad1246ad5ea02cdaf4a913
Liza Rakhina <lirakhli@microsoft.com>
2019-06-11 15:49:00 +0300

    Adding getPolicy function

--- out/token_fetch.js
+++ out/token_fetch.js
@@ -1,24 +1,55 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-console.log("Token Fetch");
-function TokenFetch() {
-    var AuthenticationContext = require("adal-node").AuthenticationContext;
-    var authorityHostUrl = "https://login.windows.net";
-    var tenant = "meganb@M365x316380.onmicrosoft.com";
-    var authorityUrl = authorityHostUrl + "/" + tenant;
-    var applicationId = "1c3e4573-2277-4eba-a240-5f0bd7804b93";
-    var clientSecret = "D-4PBO:.1+dk5jMsGs3AW+*n_iGOGZPz";
-    var resource = "00000002-0000-0000-c000-000000000000";
-    var context = new AuthenticationContext(authorityUrl);
-    context.acquireTokenWithClientCredentials(resource, applicationId, clientSecret, function (err, tokenResponse) { return (
-    /*
-    if (err) {
-        console.log("well that didn't work: " + err.stack);
-    } else {
-        console.log(tokenResponse);
-    }
-    */
-    (err) ? console.log("well that didn't work: " + err.stack) : console.log(tokenResponse)); });
+const dotenv = require("dotenv");
+const utils = require("util");
+dotenv.config();
+const fetch = require("node-fetch");
+exports.myTenant = "992d8d2f-5bb8-4131-9791-1f4b4a48e6d0";
+exports.myApplicationId = "9e6d8f78-eb08-40a6-b8ff-a9dcf9d2f038";
+function TokenFetch(tenant, applicationId) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const AuthenticationContext = require("adal-node").AuthenticationContext;
+        const authorityHostUrl = "https://login.windows.net";
+        const authorityUrl = authorityHostUrl + "/" + tenant;
+        const clientSecret = process.env.CLIENT_SECRET;
+        const resource = "https://syncservice.o365syncservice.com";
+        const context = new AuthenticationContext(authorityUrl);
+        const acquireTokenAsync = utils.promisify(context.acquireTokenWithClientCredentials).bind(context);
+        const token = yield acquireTokenAsync(resource, applicationId, clientSecret);
+        return token;
+    });
 }
 exports.TokenFetch = TokenFetch;
+function checkStatus(res) {
+    if (res.status >= 200 && res.status < 300) {
+        return res;
+    }
+    else {
+        const err = new Error(res.statusText);
+        throw err;
+    }
+}
+function getPolicy(token) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const response = yield fetch("https://dataservice.protection.outlook.com/PsorWebService/v1/ClientSyncFile/MipPolicies", {
+            headers: {
+                "Authorization": "Bearer " + token.accessToken,
+                "Content-Type": "application/xml; charset=utf-8"
+            }
+        }).then(checkStatus);
+        return response;
+    });
+}
+exports.getPolicy = getPolicy;
+TokenFetch(exports.myTenant, exports.myApplicationId).then((res) => {
+    getPolicy(res).then((res1) => { console.log(res1); });
+});
 //# sourceMappingURL=token_fetch.js.map
--- token_fetch.ts
+++ token_fetch.ts
@@ -1,21 +1,47 @@
-import * as utils from 'util';
-require('dotenv').config();
+import dotenv = require("dotenv");
+import * as utils from "util";
+dotenv.config();
 
-export async function TokenFetch(tenant : string, applicationId : string) {
+import fetch = require("node-fetch");
+
+export const myTenant = "992d8d2f-5bb8-4131-9791-1f4b4a48e6d0";
+export const myApplicationId = "9e6d8f78-eb08-40a6-b8ff-a9dcf9d2f038";
+
+export async function TokenFetch(tenant: string, applicationId: string) {
     const AuthenticationContext = require("adal-node").AuthenticationContext;
     const authorityHostUrl = "https://login.windows.net";
-    //const tenant = "992d8d2f-5bb8-4131-9791-1f4b4a48e6d0";
     const authorityUrl = authorityHostUrl + "/" + tenant;
-    //const applicationId = "9e6d8f78-eb08-40a6-b8ff-a9dcf9d2f038";
-    //const clientSecret = "6LPUheLb]tNK5*km*]SBf20D2=[ZO4r8";
     const clientSecret = process.env.CLIENT_SECRET;
     const resource = "https://syncservice.o365syncservice.com";
-
     const context = new AuthenticationContext(authorityUrl);
+    const acquireTokenAsync = utils.promisify(context.acquireTokenWithClientCredentials).bind(context);
 
-    let acquireTokenAsync = utils.promisify(context.acquireTokenWithClientCredentials).bind(context);
+    const token = await acquireTokenAsync(resource, applicationId, clientSecret);
 
-    let token = await acquireTokenAsync(resource, applicationId, clientSecret);
-
-    console.log(token);
+    return token;
 }
+
+function checkStatus(res) {
+  if (res.status >= 200 && res.status < 300) {
+    return res;
+  } else {
+    const err = new Error(res.statusText);
+    throw err;
+  }
+}
+
+export async function getPolicy(token) {
+    const response = await
+        fetch("https://dataservice.protection.outlook.com/PsorWebService/v1/ClientSyncFile/MipPolicies",
+            {
+                headers:    {
+                              "Authorization": "Bearer " + token.accessToken,
+                              "Content-Type": "application/xml; charset=utf-8"
+                            }
+            }).then(checkStatus);
+    return response;
+}
+
+TokenFetch(myTenant, myApplicationId).then((res) => {
+    getPolicy(res).then((res1) => { console.log(res1); });
+});
