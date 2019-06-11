3da68e54f3b81b57c6e22fb480870d55681d66b9
Liza Rakhina <lirakhli@microsoft.com>
2019-06-11 11:16:52 +0300

    converting TokenFetch to async

--- test.ts
+++ test.ts
@@ -1,8 +1,11 @@
 import { expect } from "chai";
 import { TokenFetch } from "./token_fetch";
 
+const tenant = "992d8d2f-5bb8-4131-9791-1f4b4a48e6d0";
+const applicationId = "9e6d8f78-eb08-40a6-b8ff-a9dcf9d2f038";
+
 describe("TokenFetch liveliness", () => {
 	it("should run", () => {
-		TokenFetch();
+		TokenFetch(tenant, applicationId);
 	});
 });
--- token_fetch.ts
+++ token_fetch.ts
@@ -1,16 +1,21 @@
-console.log("Token Fetch");
+import * as utils from 'util';
+require('dotenv').config();
 
-export function TokenFetch() {
+export async function TokenFetch(tenant : string, applicationId : string) {
     const AuthenticationContext = require("adal-node").AuthenticationContext;
     const authorityHostUrl = "https://login.windows.net";
-    const tenant = "992d8d2f-5bb8-4131-9791-1f4b4a48e6d0";
+    //const tenant = "992d8d2f-5bb8-4131-9791-1f4b4a48e6d0";
     const authorityUrl = authorityHostUrl + "/" + tenant;
-    const applicationId = "9e6d8f78-eb08-40a6-b8ff-a9dcf9d2f038";
-    const clientSecret = "6LPUheLb]tNK5*km*]SBf20D2=[ZO4r8";
+    //const applicationId = "9e6d8f78-eb08-40a6-b8ff-a9dcf9d2f038";
+    //const clientSecret = "6LPUheLb]tNK5*km*]SBf20D2=[ZO4r8";
+    const clientSecret = process.env.CLIENT_SECRET;
     const resource = "https://syncservice.o365syncservice.com";
 
     const context = new AuthenticationContext(authorityUrl);
-    context.acquireTokenWithClientCredentials(resource, applicationId, clientSecret, (err, tokenResponse) => (
-        (err) ? console.log("well that didn't work: " + err.stack) : console.log(tokenResponse)
-    ));
+
+    let acquireTokenAsync = utils.promisify(context.acquireTokenWithClientCredentials).bind(context);
+
+    let token = await acquireTokenAsync(resource, applicationId, clientSecret);
+
+    console.log(token);
 }
