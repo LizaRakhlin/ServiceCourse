06ee4e5e5f32a3f04a2985c98e698c675697abce
Liza Rakhina <lirakhli@microsoft.com>
2019-06-11 11:43:09 +0300

    Unit test for TokenFetch added

--- test.ts
+++ test.ts
@@ -1,11 +1,17 @@
-import { expect } from "chai";
+import { expect } from 'chai';
 import { TokenFetch } from "./token_fetch";
+import * as sinon from 'sinon';
+import { AuthenticationContext } from 'adal-node';
 
 const tenant = "992d8d2f-5bb8-4131-9791-1f4b4a48e6d0";
 const applicationId = "9e6d8f78-eb08-40a6-b8ff-a9dcf9d2f038";
 
 describe("TokenFetch liveliness", () => {
 	it("should run", () => {
+		let authenticationContextMock = sinon.mock(AuthenticationContext.prototype);
+		authenticationContextMock.expects('acquireTokenWithClientCredentials').once();
+
 		TokenFetch(tenant, applicationId);
+		authenticationContextMock.verify();		
 	});
 });
